32ba03e242fab2158c3b841fa330a53ceb8133e3
bhoomibalegar-sys <bhoomibalegar@gmail.com>
2026-04-26 11:04:53 +0530

    Update db.php

--- db.php
+++ db.php
@@ -2,6 +2,6 @@
 $conn = new mysqli("localhost", "root", "", "student_db");
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die(json_encode(["status"=>"error","message"=>$conn->connect_error]));
 }
 ?>
